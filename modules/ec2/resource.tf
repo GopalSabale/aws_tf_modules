9da84a5f6490d3e045acc48b9e7351eb7e2d90aa
GopalSabale <gopalsabale4@gmail.com>
2026-04-13 14:46:24 +0530

    add new instacne

--- modules/ec2/resource.tf
+++ modules/ec2/resource.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "my_ec2" {
     Name = "My-Terraform-EC2"
   }
 }
+
+resource "aws_instance" "my_ec21" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "My-Terraform-EC1"
+  }
+}
